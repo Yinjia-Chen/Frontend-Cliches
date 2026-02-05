4116968f4b5b66eca7547d3b00d402a26a8a46c3
chenyinjia39510 <chenyinjia510@hellobike.com>
2026-02-05 16:42:07 +0800

    fix(deepClone): 修复原型链丢失问题

--- 2025-下半年-手搓/01-递归深拷贝.js
+++ 2025-下半年-手搓/01-递归深拷贝.js
@@ -6,7 +6,7 @@
 const deepClone = (original_obj) => {
   if (typeof original_obj !== 'object' || original_obj === null) return original_obj;
 
-  const cloned_obj = Array.isArray(original_obj) ? [] : {}
+  const cloned_obj = Array.isArray(original_obj) ? [] : Object.create(Object.getPrototypeOf(original_obj))
 
   for (let key in original_obj) {
     if (original_obj.hasOwnProperty(key)) {
@@ -35,6 +35,16 @@ const cloned_obj = deepClone(original_deep);
 console.log(cloned_obj);
 
 console.log(deepClone(null));
+
+console.log({});
+
+class Person {
+  say() { return 'hi'; }
+}
+
+const original = new Person();
+const cloned = deepClone(original);
+cloned.say();
 
 // M1：判断是否是对象的时候要带上 null 的判断条件
 // R1：type of null === 'object'
