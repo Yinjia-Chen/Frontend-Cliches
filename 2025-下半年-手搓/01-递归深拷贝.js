1d70019a23ed5fe3695d6edb64db9e707c0e0eea
chenyinjia39510 <chenyinjia510@hellobike.com>
2026-02-05 10:32:41 +0800

    fix(deepClone): 深拷贝修复null边界问题

--- 2025-下半年-手搓/01-递归深拷贝.js
+++ 2025-下半年-手搓/01-递归深拷贝.js
@@ -4,7 +4,7 @@
  * @return {Object}
  */
 const deepClone = (original_obj) => {
-  if (typeof original_obj !== 'object') return original_obj;
+  if (typeof original_obj !== 'object' || original_obj === null) return original_obj;
 
   const cloned_obj = Array.isArray(original_obj) ? [] : {}
 
@@ -34,11 +34,13 @@ const cloned_obj = deepClone(original_deep);
 
 console.log(cloned_obj);
 
+console.log(deepClone(null));
+
 // M1：判断是否是对象的时候要带上 null 的判断条件
 // R1：type of null === 'object'
 
 // M2：非对象时返回原对象
-// R1：直接返回会导致后续递归循环出问题
+// R2：直接返回会导致后续递归循环出问题
 
 // M3：typeof obj !== 'object'
 // R3：typeof的运算结果是一个 String
